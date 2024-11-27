0ed68e154a59356b207333d368da472a510271ea
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-11-27 23:45:45 +0800

    Adding Pseodocode for the first program

---
+++ write_info_of_users.py
@@ -0,0 +1,8 @@
+#Open the text file as write
+#It should be justified as append to add more data
+#Create five input statements (e.g. Fullname, Age, Address, Birthday, and Hobby)
+#Place a try and except condition on the age as to avoid errors
+#Append all the data to the text file
+#Place another input statement asking users whether they want to enter more data or not
+#Create an if condition for the reentry input
+#End all task if the user enters no
