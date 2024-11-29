9ac253d042439421e90dd28a87ae559a171b93ea
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-11-29 13:15:14 +0800

    Opening the text file, placing the data as read, and splitting the data into separate list

--- read_info_of_users.py
+++ read_info_of_users.py
@@ -1,6 +1,13 @@
 #Open the text file as read
-#Create an input statement asking users for a name they want to look for
-#Place the data of the text file as read inside a variable
-#Split the data into separate lists
+with open("personal_info.txt", "r") as user_info:
+
+    #Create an input statement asking users for a name they want to look for
+    locator= input("Please state the name you're looking for: ")
+
+    #Place the data of the text file as read inside a variable
+    data= user_info.read()
+
+    #Split the data into separate lists
+    personal_set= data.split(" \n")
 #Create a loop for indicating whether the name is in the text file
 #Create an if condition for the data to be extracted
