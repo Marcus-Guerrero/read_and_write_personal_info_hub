6639e9aabbce1225f67671fbb493bc2f7631adfc
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-11-29 13:10:14 +0800

    Adding the pseodocode for the second program

---
+++ read_info_of_users.py
@@ -0,0 +1,6 @@
+#Open the text file as read
+#Create an input statement asking users for a name they want to look for
+#Place the data of the text file as read inside a variable
+#Split the data into separate lists
+#Create a loop for indicating whether the name is in the text file
+#Create an if condition for the data to be extracted
